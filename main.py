1d92e123f5f74f5c9ba249c7382e5457aa750b1a
Adamiik <ado.michalik@gmail.com>
2026-01-03 17:06:41 +0100

    Player draws on the screen, movement and rotation added. Objects in groups

--- main.py
+++ main.py
@@ -1,11 +1,25 @@
 import pygame
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
 from logger import log_state
+from circleshape import CircleShape
+from player import Player
 
 def main():
     pygame.init()
     
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    clock_object = pygame.time.Clock()
+    dt = 0
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
+
+    player_object = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
+
 
     while 1 == 1:
         log_state()
@@ -14,7 +28,14 @@ def main():
                 return
 
         screen.fill("black")
+        for draws in drawable:
+            draws.draw(screen)
+        
+        updatable.update(dt)
         pygame.display.flip()
+
+        clock_object.tick(60)
+        dt = (clock_object.tick(60)/1000)
     
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
     print(f"Screen width: {SCREEN_WIDTH}")
